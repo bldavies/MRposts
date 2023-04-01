4ce2312761ea7db195f1aa4e04d5363e308a5b7d
Ben Davies <git@bldavies.fastmail.com>
2023-04-01 13:32:36 -0700

    Add caching mechanism to categories.R

--- data-raw/categories.R
+++ data-raw/categories.R
@@ -18,10 +18,45 @@ source('data-raw/globals.R')
 # Import metadata
 metadata = read_csv('data-raw/metadata.csv')
 
-# Create table
-categories = list.files(POSTS_DIR, 'categories[.]csv', full.names = T, recursive = T) %>%
+# Initialize cache directory
+cache_dir = 'data-raw/categories'
+if (!dir.exists(cache_dir)) dir.create(cache_dir)
+
+# Iterate over years
+year_dirs = list.dirs(POSTS_DIR, recursive = F)
+for (year_dir in year_dirs) {
+  
+  # Iterate over months
+  month_dirs = list.dirs(year_dir, recursive = F)
+  for (month_dir in month_dirs) {
+    
+    # List post-specific files
+    files = list.files(month_dir, 'categories[.]csv', full.names = T, recursive = T)
+    
+    # Construct cache file path
+    month_ext = sub(paste0(POSTS_DIR, '/'), '', month_dir)
+    cache_file = sub('(.*)/(.*)', paste0(cache_dir, '/\\1-\\2.csv'), month_ext)
+    
+    # Create/update cache file
+    if (!file.exists(cache_file) | max(file.mtime(files)) > file.mtime(cache_file)) {
+      
+      # Create table
+      dat = files %>%
+        vroom(show_col_types = F) %>%
+        arrange(id, category)
+      
+      # Save table
+      write_csv(dat, cache_file)
+      
+    }
+  }
+}
+
+# Combine cached files into single table
+cache_files = list.files(cache_dir, full.names = T)
+categories = cache_files %>%
   vroom(show_col_types = F) %>%
-  arrange(id, category) %>%
+  arrange(id) %>%
   semi_join(metadata, by = 'id')
 
 # Save table
