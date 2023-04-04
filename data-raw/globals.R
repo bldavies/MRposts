5c821db55569e8b9e3f8ef0c7e4a1e9171a2ad17
Ben Davies <git@bldavies.fastmail.com>
2023-04-04 12:15:07 -0700

    Scrape posts from 2003

--- data-raw/globals.R
+++ data-raw/globals.R
@@ -7,6 +7,6 @@
 
 BLOG_URL = 'https://marginalrevolution.com/marginalrevolution/'
 
-DATE_RANGE = seq(as.Date('2004-01-01'), as.Date('2023-03-31'), by = 'days')
+DATE_RANGE = seq(as.Date('2003-08-01'), as.Date('2023-03-31'), by = 'days')
 
 POSTS_DIR = 'data-raw/posts/'
