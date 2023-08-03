e4db81b1714b7e0c7e5bf18f55859e7a541e13b8
Ben Davies <git@bldavies.fastmail.com>
2023-08-03 16:19:33 -0700

    Add data on posts from July 2023

--- data-raw/globals.R
+++ data-raw/globals.R
@@ -3,10 +3,10 @@
 # This script defines global constants used in other scripts.
 #
 # Ben Davies
-# July 2023
+# August 2023
 
 BLOG_URL = 'https://marginalrevolution.com/marginalrevolution/'
 
-DATE_RANGE = seq(as.Date('2003-08-01'), as.Date('2023-06-30'), by = 'days')
+DATE_RANGE = seq(as.Date('2003-08-01'), as.Date('2023-07-31'), by = 'days')
 
 POSTS_DIR = 'data-raw/posts/'
